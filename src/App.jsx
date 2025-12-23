475663a91b91d9fbc4cca69149b7c3473570b486
Pravesh Singh <Pravesh.singh@gmail.com>
2025-12-23 22:35:09 +0530

    Final code

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+
+function App() {
+
+  return (
+    <>
+    {/* Button */}
+
+    <button className="px-6 py-3 bg-indigo-600 text-white rounded-full shadow-lg hover:bg-indigo-700 transition">
+      Subscribe
+    </button>
+
+   {/* Card UI Everyone Can Build */}
+    {/* <div className="max-w-sm bg-white rounded-2xl shadow-xl p-6">
+      <h2 className="text-2xl font-bold mb-2">Tailwind Card</h2>
+      <p className="text-gray-600 mb-4">
+        Build modern UI without writing custom CSS.
+      </p>
+      <button className="text-indigo-600 font-semibold hover:underline">
+        Learn More →
+      </button>
+    </div> */}
+
+    {/* Responsive Boxes – Resize Magic */}
+    {/* <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+      {[1,2,3,4,5,6].map(n => (
+        <div key={n} className="bg-indigo-100 p-6 rounded-xl text-center font-semibold">
+          Box {n}
+        </div>
+      ))}
+    </div> */}
+
+    {/* Simple Navbar = Real Website Feel  */}
+    {/* <nav className="flex items-center justify-between px-6 py-4 bg-slate-900 text-white">
+      <h1 className="text-xl font-bold">MySite</h1>
+      <div className="space-x-4">
+        <a className="hover:text-indigo-400" href="#">Home</a>
+        <a className="hover:text-indigo-400" href="#">About</a>
+        <a className="hover:text-indigo-400" href="#">Contact</a>
+      </div>
+    </nav> */}
+
+    {/* Hover Animation  */}
+    {/* <div className="p-10 bg-indigo-500 text-white rounded-2xl text-xl font-bold 
+                    transform hover:scale-105 transition duration-300">
+      Hover Me 🚀
+    </div> */}
+
+    </>
+  )
+}
+
+export default App
